70cbfce70afe64665e6848cc7e56f41c87dee6f4
James Prentice <jamesprenticez@gmail.com>
2022-11-12 17:53:51 +1300

    99% searchSelect

--- frontend/client/components/common/Select.jsx
+++ frontend/client/components/common/Select.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useRef} from 'react'
 import { useKeyPressed } from "../../hooks/useKeyPressed"
 
 export default function Select({options, value, setSelectedValue, placeholder}) {
-  const containerRef = useRef(null)
+  const inputRef = useRef(null)
   
   const [isOpen, setIsOpen] = useState(false)
   const [filteredArray, setFilteredArray] = useState(options)
@@ -12,30 +12,32 @@ export default function Select({options, value, setSelectedValue, placeholder}) 
   const downArrowPressed = useKeyPressed("ArrowDown")
   const enterPressed = useKeyPressed("Enter")
 
-  
-
   useEffect(() => {
     if(isOpen) return
-    if(downArrowPressed && !isOpen) {setIsOpen(true),  containerRef.current.focus()}
+    if(downArrowPressed && !isOpen) {setIsOpen(true),  inputRef.current.focus()}
     enterPressed //prevent default so contentEditable doesnt create a new line in the input box
   }, [downArrowPressed, enterPressed])
 
   const handleArrowKeys = (e) => {
     switch(e.key){
       case "ArrowUp":
-        setActiveIndex(activeIndex === 0 ? filteredArray.length - 1 : activeIndex - 1) // If the index goes below 0, wrap around
+          //setActiveIndex(activeIndex === 0 ? filteredArray.length - 1 : activeIndex - 1) // If the index goes below 0, wrap around
+          setActiveIndex(prevState => (prevState > 0 ? prevState - 1 : prevState))
         break
-      case "ArrowDown":
-        setActiveIndex(activeIndex === filteredArray.length - 1 ? 0 : activeIndex + 1) // If the index goes above the length of the array, wrap around
+        case "ArrowDown":
+          //setActiveIndex(activeIndex === filteredArray.length - 1 ? 0 : activeIndex + 1) // If the index goes above the length of the array, wrap around
+          setActiveIndex(prevState => prevState < filteredArray.length - 1 ? prevState + 1 : prevState)
         break
       case "Enter":
         //alert(`Selected ${filteredArray[activeIndex]}`)
         setSelectedValue(filteredArray[activeIndex])
-        console.log(filteredArray[activeIndex])
+        setSearchValue("")
+        setIsOpen(false)
+        inputRef.current.blur()
         break
       case "Escape":
         setIsOpen(false)
-        containerRef.current.blur()
+        inputRef.current.blur()
         break
     }
   }
@@ -46,6 +48,7 @@ export default function Select({options, value, setSelectedValue, placeholder}) 
   }
   
   const handleUpdate = (e) => {
+    setActiveIndex(0)
     setSearchValue(e.target.value)
     updateFilteredArray(e.target.value)
   }
@@ -53,16 +56,14 @@ export default function Select({options, value, setSelectedValue, placeholder}) 
   return (
     <>
     <div
-      ref={containerRef}
       className="
       relative
       my-1
       bg-theme-primary
       text-theme-secondary
       w-full
-      py-2       
-      px-12
-      outline-none
+      py-2
+      px-[3rem]       
       ring-[0.05rem]
       ring-theme-tertiary
       focus:ring-theme-quaternary
@@ -71,24 +72,32 @@ export default function Select({options, value, setSelectedValue, placeholder}) 
       "
       onClick={() => setIsOpen(true)}
       onBlur={() => setIsOpen(false)}
-      tabIndex={0}
+      // tabIndex={0}
     >
-      {/* <div className="bg-red-500 w-[3rem] absolute inset-y-0 left-0 flex items-center justify-center pointer-events-none ">
+      {/* <div className="bg-red-500 w-[3rem] flex items-center justify-center pointer-events-none ">
         <svg height={22} width={22} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>     
       </div> */}
+      {/* {console.log("searchvalue", searchValue)} */}
 
-      <p>{value.label}</p>
+      <div className='h-[1rem] py-2'>
 
-      <input  
-        type="text"
-        onKeyDown={(e) => handleArrowKeys(e)}
-        value={searchValue}
-        placeholder={placeholder || "Select..."}
-        className="bg-red-500"
-        onChange={(e) => handleUpdate(e)}
-      />
+        <input 
+          ref={inputRef} 
+          type="text"
+          onKeyDown={(e) => handleArrowKeys(e)}
+          value={searchValue}
+          className='bg-transparent py-2 absolute left-[3rem] right-[3rem] inset-y-0 outline-none z-50'
+          onChange={(e) => handleUpdate(e)}
+        />
+
+        <p 
+          className='absolute flex items-center left-[3rem] right-[3rem] inset-y-0 pointer-events-none'
+          >
+            {searchValue.length > 0 ? "" : value.label.length > 0 ? value.label : placeholder || "Select..."}
+        </p>
+      </div>
 
       {/*
       <div 
